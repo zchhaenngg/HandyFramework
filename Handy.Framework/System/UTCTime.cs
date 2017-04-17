bdcaacfa03fe5b50d6641c4682e36cec1a13cb52
zchhaenngg <1076968967@qq.com>
2017-04-17 23:52:57 +0800

    UTCTime,Decorated DateTime.

---
+++ Handy.Framework/System/UTCTime.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handy.Framework.System
+{
+    /// <summary>
+    /// Decorated DateTime,Value representing the time after DateTime converted to UTC.
+    /// </summary>
+    public struct UTCTime
+    {
+        /// <summary>
+        /// Decorate time
+        /// </summary>
+        /// <param name="time"></param>
+        public UTCTime(DateTime time) : this()=> Value = time;
+
+        private DateTime _datetime;
+        /// <summary>
+        /// UTC Time
+        /// </summary>
+        public DateTime Value
+        {
+            get
+            {
+                return _datetime;
+            }
+            set
+            {
+
+                if (value.Kind == DateTimeKind.Unspecified)
+                {
+                    if (value == DateTime.MinValue)
+                    {
+                        _datetime = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc);
+                    }
+                    else
+                    {
+                        throw new Exception("Failed to convert the value to UTC time,for the value's DateTimeKind is Unspecified!");
+                    }
+                }
+                _datetime = value.ToUniversalTime();
+            }
+        }
+
+        /// <summary>
+        /// now
+        /// </summary>
+        public static UTCTime Now => new UTCTime(DateTime.UtcNow);
+
+        /// <summary>
+        /// Converting time implicitly to UTC
+        /// </summary>
+        /// <param name="time"></param>
+        public static implicit operator UTCTime(DateTime time) => new UTCTime(time);
+        /// <summary>
+        /// Converting time implicitly to UTC
+        /// </summary>
+        /// <param name="time"></param>
+
+        public static implicit operator UTCTime?(DateTime? time)
+        {
+            if (time == null)
+            {
+                return null;
+            }
+            else
+            {
+                return time.Value;
+            }
+        }
+        /// <summary>
+        /// Converting time implicitly to UTC.
+        /// </summary>
+        /// <param name="time"></param>
+        public static explicit operator UTCTime(DateTime? time)
+        {
+            if (time == null)
+            {
+                throw new ArgumentNullException(nameof(time));
+            }
+            else
+            {
+                return time.Value;
+            }
+
+        }
+        /// <summary>
+        /// Gets the DateTime represented by UTCTime.
+        /// </summary>
+        /// <param name="time"></param>
+        public static implicit operator DateTime(UTCTime time)=> time.Value;
+        /// <summary>
+        /// Gets the DateTime represented by UTCTime.
+        /// </summary>
+        /// <param name="time"></param>
+        public static implicit operator DateTime? (UTCTime? time)=> time?.Value;
+        /// <summary>
+        /// Gets the DateTime represented by UTCTime.
+        /// </summary>
+        /// <param name="time"></param>
+        public static explicit operator DateTime(UTCTime? time)
+        {
+            if (time == null)
+            {
+                throw new ArgumentNullException(nameof(time));
+            }
+            else
+            {
+                return time.Value;
+            }
+
+        }
+    }
+}
--- Handy.TestFramework/Extensions/TypeExtensionTest.cs
+++ Handy.TestFramework/Extensions/TypeExtensionTest.cs
@@ -10,10 +10,11 @@ namespace Handy.TestFramework.Extensions
         [TestMethod]
         public void IsString()
         {
-            var trueS = "dd";
-            var falseB = false;
-            Assert.IsTrue(trueS.GetType().IsString());
-            Assert.IsFalse(falseB.GetType().IsString());
+            var t1 = DateTime.MinValue;
+            var t = DateTime.MinValue.ToUniversalTime();
+            var t2 = DateTime.Parse("2017-09-12");
+             var t3 = t2.ToUniversalTime();
+            Assert.AreEqual(t1, t);
         }
     }
 }
